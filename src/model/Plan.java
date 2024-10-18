dd5974739d427d9b075a8cda809cc72f1e547423
edugast <eleonore.dugast@insa-lyon.fr>
2024-10-18 11:57:30 +0200

    plan methodes qui marchent surement pas encore

--- src/model/Plan.java
+++ src/model/Plan.java
@@ -1,13 +1,32 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+import model.Intersection;
+import model.Troncon;
+
 public class Plan {
     private List<Intersection> intersections;
     private List<Troncon> troncons;
 
     // Constructeur
-    public Plan(List<Intersection> intersections, List<Troncon> troncons) {
-        this.intersections = intersections;
-        this.troncons = troncons;
+    public Plan() {
+        this.intersections = new ArrayList<Intersection>;
+        this.troncons = new ArrayList<Troncons>;
+    }
+
+    public void ajouterIntersection(Intersection intersection){
+        this.intersections.add(intersection);
+    }
+
+    public Intersection chercherIntersectionParId(Long id){
+        Intersection inter;
+        for (int i = 0; i<intersections.size();i++){
+            if (intersections[i].getId()==id){
+                inter = intersections[i];
+            }
+        }
+        return inter;
     }
 
 }
