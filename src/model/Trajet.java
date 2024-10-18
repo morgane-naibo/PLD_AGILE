3a80bee01f82a70ca8ddf7f70fae6a1ad5627354
Morgane <142242967+morgane-naibo@users.noreply.github.com>
2024-10-18 11:25:37 +0200

    toutes les classes à revoir

--- src/model/Trajet.java
+++ src/model/Trajet.java
@@ -1,5 +1,15 @@
 package model;
 
 public class Trajet {
+    private Intersection depart;
+    private Intersection arrivee;
+    private double distance;
+
+    // Constructeur
+    public Trajet(Intersection depart, Intersection arrivee, double distance) {
+        this.depart = depart;
+        this.arrivee = arrivee;
+        this.distance = distance;
+    }
 
 }
--- src/model/Troncon.java
+++ src/model/Troncon.java
@@ -1,5 +1,17 @@
 package model;
 
 public class Troncon {
+    private Intersection intersectionDepart;
+    private Intersection intersectionArrivee;
+    private double longueur;
+    private String nomRue;
+
+    // Constructeur
+    public Troncon(Intersection intersectionDepart, Intersection intersectionArrivee, double longueur, String nomRue) {
+        this.intersectionDepart = intersectionDepart;
+        this.intersectionArrivee = intersectionArrivee;
+        this.longueur = longueur;
+        this.nomRue = nomRue;
+    }
 
 }
