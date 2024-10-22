edb44b9de0fcc61a989f0e9cdb56952de79b3189
Xx-DarkBGdu69-xX <marcsauce02@gmail.com>
2024-10-22 09:20:26 +0200

    Debut XML Demande

--- src/model/Entrepot.java
+++ src/model/Entrepot.java
@@ -3,10 +3,11 @@ package model;
 
 public class Entrepot extends Intersection{
    
-
+    private String heureDepart;
     // Constructeur
-    public Entrepot(long id, double latitude, double longitude) {
-     super(id, latitude, longitude);
+    public Entrepot(long id, String heure) {
+     this.setId(id);
+     this.heureDepart = heure;
     }
 
     public Entrepot() {
