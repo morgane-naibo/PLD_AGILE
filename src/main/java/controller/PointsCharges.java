3d55aea4c3635ae5313701bea02a932d29f2b914
Xx-DarkBGdu69-xX <marcsauce02@gmail.com>
2024-11-05 11:54:41 +0100

    Etats incoming

---
+++ src/main/java/controller/PointsCharges.java
@@ -0,0 +1,15 @@
+package controller;
+
+public class PointsCharges extends Etat {
+    @Override
+    public void entrerEtat() {
+        System.out.println("Entrée dans l'état d'accueil.");
+    }
+
+    @Override
+    public void handleActionA() {
+        System.out.println("Action A exécutée dans l'état d'accueil.");
+    }
+
+    // Pas de surcharge de handleActionB ou handleActionC car elles ne sont pas disponibles dans cet état
+}
