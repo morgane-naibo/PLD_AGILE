dd125eb34bdfbb5168bf4a83e332e76b45f4f695
cmonluc <constance.monluc@gmail.com>
2024-11-15 12:45:20 +0100

    javadoc Intersection.java

--- src/main/java/model/Intersection.java
+++ src/main/java/model/Intersection.java
@@ -3,14 +3,35 @@ package model;
 import java.util.List;
 import java.util.ArrayList;
 
+/**
+ * Classe représentant une intersection dans le système.
+ * Une intersection est définie par une latitude, une longitude, un identifiant unique, 
+ * une liste de tronçons associés et un numéro optionnel.
+ */
 public class Intersection {
+
+    /** Latitude de l'intersection. */
     protected double latitude;
+
+    /** Longitude de l'intersection. */
     protected double longitude;
+
+    /** Identifiant unique de l'intersection. */
     protected long id;
+
+    /** Liste des tronçons connectés à cette intersection. */
     protected List<Troncon> listeTroncons;
+
+    /** Numéro optionnel de l'intersection, utilisé pour des références spécifiques. */
     protected int numero;
 
-    // Constructeurs
+    /**
+     * Constructeur avec tous les paramètres principaux.
+     * 
+     * @param id l'identifiant unique de l'intersection.
+     * @param latitude la latitude de l'intersection.
+     * @param longitude la longitude de l'intersection.
+     */
     public Intersection(long id, double latitude, double longitude) {
         this.id = id;
         this.latitude = latitude;
@@ -18,6 +39,12 @@ public class Intersection {
         this.listeTroncons = new ArrayList<Troncon>();
     }
 
+    /**
+     * Constructeur par copie.
+     * Crée une nouvelle intersection avec les mêmes attributs qu'une intersection existante.
+     * 
+     * @param i l'intersection à copier.
+     */
     public Intersection(Intersection i) {
         this.id = i.id;
         this.latitude = i.latitude;
@@ -25,61 +52,123 @@ public class Intersection {
         this.listeTroncons = i.listeTroncons;
     }
 
+    /**
+     * Constructeur par défaut.
+     * Initialise une intersection sans paramètres spécifiques.
+     */
     public Intersection() {
     }
 
-    //getters
+    /**
+     * Récupère la latitude de l'intersection.
+     * 
+     * @return la latitude de l'intersection.
+     */
     public double getLatitude() {
         return this.latitude;
     }
 
+    /**
+     * Récupère la longitude de l'intersection.
+     * 
+     * @return la longitude de l'intersection.
+     */
     public double getLongitude() {
         return this.longitude;
     }
 
+    /**
+     * Récupère l'identifiant unique de l'intersection.
+     * 
+     * @return l'identifiant de l'intersection.
+     */
     public long getId() {
         return this.id;
     }
 
-    public List<Troncon> getListeTroncons(){
+    /**
+     * Récupère la liste des tronçons connectés à l'intersection.
+     * 
+     * @return une liste de tronçons connectés.
+     */
+    public List<Troncon> getListeTroncons() {
         return this.listeTroncons;
     }
 
-    public int getNumero(){
+    /**
+     * Récupère le numéro de l'intersection.
+     * 
+     * @return le numéro de l'intersection.
+     */
+    public int getNumero() {
         return this.numero;
     }
 
-    //setters
+    /**
+     * Modifie la latitude de l'intersection.
+     * 
+     * @param newLatitude la nouvelle latitude à attribuer.
+     */
     public void setLatitude(double newLatitude) {
         this.latitude = newLatitude;
     }
 
+    /**
+     * Modifie la longitude de l'intersection.
+     * 
+     * @param newLongitude la nouvelle longitude à attribuer.
+     */
     public void setLongitude(double newLongitude) {
         this.longitude = newLongitude;
     }
 
-    public void setId(long newId){
+    /**
+     * Modifie l'identifiant unique de l'intersection.
+     * 
+     * @param newId le nouvel identifiant unique.
+     */
+    public void setId(long newId) {
         this.id = newId;
     }
 
+    /**
+     * Modifie la liste des tronçons connectés à l'intersection.
+     * 
+     * @param newListeTroncons la nouvelle liste de tronçons.
+     */
     public void setListeTroncons(List<Troncon> newListeTroncons) {
         this.listeTroncons = newListeTroncons;
     }
 
-    public void setNumero(int num){
+    /**
+     * Modifie le numéro de l'intersection.
+     * 
+     * @param num le nouveau numéro à attribuer.
+     */
+    public void setNumero(int num) {
         this.numero = num;
     }
 
-    //toString
+    /**
+     * Retourne une représentation sous forme de chaîne de caractères de l'intersection.
+     * Cette représentation inclut uniquement l'identifiant de l'intersection.
+     * 
+     * @return une chaîne de caractères représentant l'intersection.
+     */
     @Override
     public String toString() {
-        //return "Intersection [id=" + id + ", latitude=" + latitude + ", longitude=" + longitude + "]" ;
         return "id " + id;
     }
 
-    //autres methodes
+    /**
+     * Ajoute un tronçon à la liste des tronçons connectés à l'intersection.
+     * 
+     * @param troncon le tronçon à ajouter.
+     */
     public void ajouterTroncon(Troncon troncon) {
         this.listeTroncons.add(troncon);
     }
+
+    // Méthodes supplémentaires à implémenter selon les besoins spécifiques.
 
 }
